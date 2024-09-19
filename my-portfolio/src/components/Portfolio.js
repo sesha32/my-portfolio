d50bed7f27e7ae2022db498b356a65a6fe1a336e
Sesha <seshasatyasai2003@gmail.com>
2024-09-19 22:50:24 +0530

    third commit

--- my-portfolio/src/components/Portfolio.js
+++ my-portfolio/src/components/Portfolio.js
@@ -18,15 +18,30 @@ const Portfolio = () => {
       {/* Home Section */}
       <section id="home" className="home-section">
         <div className="left-section">
-          <h1>Sesha satya sai Puvvala</h1>
-          <p><strong>Career Objective:</strong>Proficient in application development, encompassing 
-both front-end and back-end technologies. Interested in 
-Back-end. Skilled in database management and 
-maintaining databases for optimal performance. 
-Enthusiastic about cybersecurity and networking, 
-continuously seeking opportunities to expand knowledge 
-and contribute to secure digital environments.</p>
-          <p><strong>Educational Information:</strong> Your educational background details go here.</p>
+          <h1>Sesha Satya Sai Puvvala</h1>
+          <h3>Career Objective:</h3>
+          <p>Proficient in application development, encompassing both front-end and back-end technologies. Interested in Back-end. Skilled in database management and maintaining databases for optimal performance. Enthusiastic about cybersecurity and networking, continuously seeking opportunities to expand knowledge and contribute to secure digital environments.</p>
+
+          <h3>Educational Information:</h3>
+          <div className="education-info">
+            <div className="education-item">
+              <p className="course"><strong>B.Tech in Computer Science & Engineering</strong></p>
+              <p className="institution">Jawaharlal Nehru Technological University-Gurajada, Vizianagaram</p>
+              <p className="date">2022-2025</p>
+            </div>
+
+            <div className="education-item">
+              <p className="course"><strong>Diploma in Computer Engineering</strong></p>
+              <p className="institution">Andhra Polytechnic College</p>
+              <p className="date">2019-2022</p>
+            </div>
+
+            <div className="education-item">
+              <p className="course"><strong>Xth</strong></p>
+              <p className="institution">Sri Chaitanya School</p>
+              <p className="date">2019</p>
+            </div>
+          </div>
         </div>
         <div className="right-section">
           <img src={require('../assets/profile.jpg')} alt="Your Photo" />
@@ -34,7 +49,7 @@ and contribute to secure digital environments.</p>
       </section>
 
       {/* Other Sections */}
-      <section id="projects" className="section"> 
+      <section id="projects" className="section">
         <h2>Projects</h2>
         {/* Add project details */}
       </section>
