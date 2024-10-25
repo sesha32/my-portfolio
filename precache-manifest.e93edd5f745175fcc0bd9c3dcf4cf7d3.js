2b6e83bad67c4520cb72f04e54483d2a6dd81f67
Sesha <seshasatyasai2003@gmail.com>
2024-10-25 16:37:52 +0530

    Updates

---
+++ precache-manifest.e93edd5f745175fcc0bd9c3dcf4cf7d3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e7b67d4b961e07c37a9b1a02a96a895",
+    "url": "/my-portfolio/index.html"
+  },
+  {
+    "revision": "4fc70d72ea1125a1b071",
+    "url": "/my-portfolio/static/css/main.58447340.chunk.css"
+  },
+  {
+    "revision": "d397cec7fa35e38e0798",
+    "url": "/my-portfolio/static/js/2.6692351d.chunk.js"
+  },
+  {
+    "revision": "4fc70d72ea1125a1b071",
+    "url": "/my-portfolio/static/js/main.f46749ce.chunk.js"
+  },
+  {
+    "revision": "f2d15970f74596a69ca7",
+    "url": "/my-portfolio/static/js/runtime~main.674e5ed7.js"
+  },
+  {
+    "revision": "4fc2cbade5cc31810515f797027fc702",
+    "url": "/my-portfolio/static/media/profile.4fc2cbad.jpg"
+  }
+]);
